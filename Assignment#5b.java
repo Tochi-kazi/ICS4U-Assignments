d796b231269218a37dd83b437701555b6ea0b10e
Tochi <31775683+Tochi-kazi@users.noreply.github.com>
2019-05-09 13:51:49 -0400

    Update Assignment#5b.java

--- Assignment#5b.java
+++ Assignment#5b.java
@@ -1,7 +1,6 @@
 /**Name: Tochi Iroakazi
  * Subject: ICS4U
  * Description: This program Develops a symmetri pattern about n where each half before and after n is also symmetric around n-1.
- * 
  */
 
 
@@ -27,32 +26,21 @@ public class RecursionB {
 	}
 	public static String symmetricPattern(int num) {
         
-        // make a variable to hold the text for where the whole pattern is contained (output)
-        String output = "";
-        // make a variable to hold the text for where the repetitions (1 2 1) is contained (nextOutput)
+        String output = Integer.toString(num);
+
         String nextOuput;
         
-        if(num >= 15 || num < 1) {
-            // returns error if number is greater than/equal to 15 or number is less than 1
-            return "Error";
-        }else if(num == 1) {
-        	//prints out " 1 " if number is 1
-            return " 1 ";
-        }else {
-            // for numbers greater than one but less or equal to 15
-            nextOuput = symmetricPattern(num - 1);
-            // assign recursion to variable nextOutput so you don't need to type in that same line of code over and over
-            if(num <= 5) {
-                //if number is less than or equal to 5, let the output be assigned nextOuput (recursion) + number + nextOutput(recursion)
-                output = nextOuput + num + nextOuput;
-            }
-            else {
-                //if the number is greater than 5, do the same thing from if statement, except add a new line before the next recursion (new line after each int greater than 5)
-                output = nextOuput + num + nextOuput;
-            }
-            //return output after if-else
-            return output;
-        }
+        String result;
+        
+        
+       if (num == 1) {
+    	   result = output;
+       }else {
+    	   result = symmetricPattern(num - 1)  + " " + output  + " " + symmetricPattern(num - 1);
+       }
+       return result;
+      
+        
     }
 
 }
